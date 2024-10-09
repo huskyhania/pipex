6307781a77b886fd599820bdf14d9b26863ed2a8
Hanna Skrzypiec <hskrzypi@c2r6p11.hive.fi>
2024-10-09 21:36:33 +0300

    working path

--- main.c
+++ main.c
@@ -6,30 +6,104 @@
 /*   By: hskrzypi <hskrzypi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 14:05:21 by hskrzypi          #+#    #+#             */
-/*   Updated: 2024/10/01 20:41:55 by hskrzypi         ###   ########.fr       */
+/*   Updated: 2024/10/09 21:07:53 by hskrzypi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 #include <stdio.h>
 
-int pipex(char *file1, char *cmd1[], char *cmd2[], char *file2)
+char *join_paths(const char *dir, const char *cmd)
+{
+    size_t dir_len = ft_strlen(dir);
+    size_t cmd_len = ft_strlen(cmd);
+    char *full_path = malloc(dir_len + cmd_len + 2); // +2 for "/" and null terminator
+    if (!full_path)
+        return NULL;
+    ft_strlcpy(full_path, dir, dir_len + 1);
+    full_path[dir_len] = '/';
+    ft_strlcpy(full_path + dir_len + 1, cmd, cmd_len + 1);
+    return (full_path);
+}
+
+char *find_path_in_envp(char *envp[])
+{
+    int i = 0;
+    while (envp[i] != NULL)
+    {
+        if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+        	return (envp[i] + 5);
+        i++;
+    }
+    return NULL; // Return NULL if PATH is not found
+}
+
+char *get_command_path(const char *cmd, char *envp[])
+{
+	if (access(cmd, X_OK) == 0)
+		return (ft_strdup(cmd));
+	char	*path_env = find_path_in_envp(envp);
+	if (!path_env)
+		return (NULL);
+	char	**directories = ft_split(path_env, ':');
+	if (!directories)
+		return (NULL);
+	int i = 0;
+	while (directories[i] != NULL)
+	{
+		char *full_path = join_paths(directories[i], cmd);
+		if (!full_path)
+		{
+			int j = 0;
+			while (directories[j] != NULL)
+			{
+				free(directories[j]);
+				j++;
+			}
+			free(directories);
+			return (NULL);
+		}
+		if (access(full_path, X_OK) == 0)
+		{
+			int j = 0;
+			while (directories[j] != NULL)
+			{
+				free(directories[j]);
+				j++;
+			}
+			free(directories);
+			return (full_path);
+		}
+		free(full_path);
+		i++;
+	}
+	int k = 0;
+	while (directories[k] != NULL)
+	{
+		free(directories[k]);
+		k++;
+	}
+	free(directories);
+	return (NULL);
+}
+
+int pipex(char *file1, char *cmd1[], char *cmd2[], char *file2, char **envp)
 {
 	int	fd[2];
 	int	pid1;
 	int	pid2;
 	int	input_fd;
 	int	output_fd;
-
+	char	*cmd_path;
 	if (pipe(fd) == -1)
 	{
-		perror("opening pipe error\n");
+		perror("opening pipe error");
 		exit(EXIT_FAILURE);
 	}
 	pid1 = fork();
 	if (pid1 < 0)
 	{
-		perror("fork error\n");
+		perror("fork error");
 		exit(EXIT_FAILURE);
 	}
 	if (pid1 == 0) //first child process for 1st command
@@ -37,22 +111,29 @@ int pipex(char *file1, char *cmd1[], char *cmd2[], char *file2)
 		input_fd = open(file1, O_RDONLY);
 		if (input_fd < 0)
 		{
-			perror("file1 open error\n");
+			perror("file1 open error");
 			exit(EXIT_FAILURE);
 		}
 		dup2(input_fd, STDIN_FILENO);
 		dup2(fd[1], STDOUT_FILENO);
+		close(input_fd);
 		close(fd[0]);
 		close(fd[1]);
-		close(input_fd);
-		execve(cmd1[0], cmd1, NULL);
-		perror("execve cmd1 error\n");
+		cmd_path = get_command_path(cmd1[0], envp);
+		if (!cmd_path)
+		{
+			perror("Command 1 not found");
+			exit(EXIT_FAILURE);
+		}
+		execve(cmd_path, cmd1, envp);
+		free(cmd_path);
+		perror("execve cmd1 error");
 		exit(EXIT_FAILURE);
 	}
 	pid2 = fork();
 	if (pid2 < 0)
 	{
-		perror("2nd fork error\n");
+		perror("2nd fork error");
 		exit(EXIT_FAILURE);
 	}
 	if (pid2 == 0) // second child process for 2nd command
@@ -60,7 +141,7 @@ int pipex(char *file1, char *cmd1[], char *cmd2[], char *file2)
 		output_fd = open(file2, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		if (output_fd < 0)
 		{
-			perror("file2 open error\n");
+			perror("file2 open error");
 			exit(EXIT_FAILURE);
 		}
 		dup2(fd[0], STDIN_FILENO);
@@ -68,8 +149,15 @@ int pipex(char *file1, char *cmd1[], char *cmd2[], char *file2)
 		close(fd[1]);
 		close(fd[0]);
 		close(output_fd);
-		execve(cmd2[0], cmd2, NULL);
-		perror("execve cmd2 error\n");
+		cmd_path = get_command_path(cmd2[0], envp);
+		if (!cmd_path)
+		{
+			perror("Command 2 not found");
+			exit(EXIT_FAILURE);
+		}
+		execve(cmd_path, cmd2, NULL);
+		free(cmd_path);
+		perror("execve cmd2 error");
 		exit(EXIT_FAILURE);
 	}
 	close(fd[0]);
@@ -79,7 +167,7 @@ int pipex(char *file1, char *cmd1[], char *cmd2[], char *file2)
 	return (0);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char **argv, char **envp)
 {
 	if (argc != 5)
 	{
@@ -88,6 +176,6 @@ int main(int argc, char **argv)
 	}
 	char *cmd1[] = {argv[2], NULL};
 	char *cmd2[] = {argv[3], NULL};
-	pipex(argv[1], cmd1, cmd2, argv[4]);
+	pipex(argv[1], cmd1, cmd2, argv[4], envp);
 	return (0);
 }
---
+++ pipex.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hskrzypi <hskrzypi@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/27 14:06:12 by hskrzypi          #+#    #+#             */
+/*   Updated: 2024/10/09 21:09:27 by hskrzypi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "./complete_Libft/libft.h"
+# include <unistd.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+
+#endif
