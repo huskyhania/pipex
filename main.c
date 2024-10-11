68ef67c9828f06da0908967ad26fcc2fe9502499
Hanna Skrzypiec <hskrzypi@c3r2p7.hive.fi>
2024-10-11 20:28:40 +0300

    fixed leaks, commands with args

--- main.c
+++ main.c
@@ -6,86 +6,12 @@
 /*   By: hskrzypi <hskrzypi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 14:05:21 by hskrzypi          #+#    #+#             */
-/*   Updated: 2024/10/09 21:07:53 by hskrzypi         ###   ########.fr       */
+/*   Updated: 2024/10/11 18:10:15 by hskrzypi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 #include <stdio.h>
-
-char *join_paths(const char *dir, const char *cmd)
-{
-    size_t dir_len = ft_strlen(dir);
-    size_t cmd_len = ft_strlen(cmd);
-    char *full_path = malloc(dir_len + cmd_len + 2); // +2 for "/" and null terminator
-    if (!full_path)
-        return NULL;
-    ft_strlcpy(full_path, dir, dir_len + 1);
-    full_path[dir_len] = '/';
-    ft_strlcpy(full_path + dir_len + 1, cmd, cmd_len + 1);
-    return (full_path);
-}
-
-char *find_path_in_envp(char *envp[])
-{
-    int i = 0;
-    while (envp[i] != NULL)
-    {
-        if (ft_strncmp(envp[i], "PATH=", 5) == 0)
-        	return (envp[i] + 5);
-        i++;
-    }
-    return NULL; // Return NULL if PATH is not found
-}
-
-char *get_command_path(const char *cmd, char *envp[])
-{
-	if (access(cmd, X_OK) == 0)
-		return (ft_strdup(cmd));
-	char	*path_env = find_path_in_envp(envp);
-	if (!path_env)
-		return (NULL);
-	char	**directories = ft_split(path_env, ':');
-	if (!directories)
-		return (NULL);
-	int i = 0;
-	while (directories[i] != NULL)
-	{
-		char *full_path = join_paths(directories[i], cmd);
-		if (!full_path)
-		{
-			int j = 0;
-			while (directories[j] != NULL)
-			{
-				free(directories[j]);
-				j++;
-			}
-			free(directories);
-			return (NULL);
-		}
-		if (access(full_path, X_OK) == 0)
-		{
-			int j = 0;
-			while (directories[j] != NULL)
-			{
-				free(directories[j]);
-				j++;
-			}
-			free(directories);
-			return (full_path);
-		}
-		free(full_path);
-		i++;
-	}
-	int k = 0;
-	while (directories[k] != NULL)
-	{
-		free(directories[k]);
-		k++;
-	}
-	free(directories);
-	return (NULL);
-}
 
 int pipex(char *file1, char *cmd1[], char *cmd2[], char *file2, char **envp)
 {
@@ -174,8 +100,15 @@ int main(int argc, char **argv, char **envp)
 		write(2, "Params should be: file1 cmd1 cmd2 file2\n", 40);
 		return (1);
 	}
-	char *cmd1[] = {argv[2], NULL};
-	char *cmd2[] = {argv[3], NULL};
+	char **cmd1 = ft_split(argv[2], ' ');
+	char **cmd2 = ft_split(argv[3], ' ');
+	if (!cmd1 || !cmd2)
+	{
+		perror("Command parsing failed");
+		return (1);
+	}
 	pipex(argv[1], cmd1, cmd2, argv[4], envp);
+	free_array(cmd1);
+	free_array(cmd2);
 	return (0);
 }
---
+++ paths.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   paths.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hskrzypi <hskrzypi@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/11 16:04:33 by hskrzypi          #+#    #+#             */
+/*   Updated: 2024/10/11 17:08:00 by hskrzypi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+#include <stdio.h>
+
+char *join_paths(const char *dir, const char *cmd)
+{
+    size_t dir_len = ft_strlen(dir);
+    size_t cmd_len = ft_strlen(cmd);
+    char *full_path = malloc(dir_len + cmd_len + 2); // +2 for "/" and null terminator
+    if (!full_path)
+        return NULL;
+    ft_strlcpy(full_path, dir, dir_len + 1);
+    full_path[dir_len] = '/';
+    ft_strlcpy(full_path + dir_len + 1, cmd, cmd_len + 1);
+    return (full_path);
+}
+
+char *find_path_in_envp(char *envp[])
+{
+    int i;
+    i = 0;
+    while (envp[i] != NULL)
+    {
+        if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+        	return (envp[i] + 5);
+        i++;
+    }
+    errno = ENOENT;
+    return (NULL); // Return NULL if PATH is not found
+}
+
+void free_array(char **array)
+{
+	int	i;
+
+	i = 0;
+	if (!array)
+		return;
+	while (array[i] != NULL)
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+	return;
+}
+
+char *get_command_path(const char *cmd, char *envp[])
+{
+	char *path_env;
+	char	**directories;
+	int	i;
+	char *full_path;
+	
+	if (access(cmd, X_OK) == 0)
+		return (ft_strdup(cmd));
+	path_env = find_path_in_envp(envp);
+	if (!path_env)
+		return (NULL);
+	directories = ft_split(path_env, ':');
+	if (!directories)
+		return (NULL);
+	i = 0;
+	while (directories[i] != NULL)
+	{
+		full_path = join_paths(directories[i], cmd);
+		if (!full_path)
+		{
+			free_array(directories);
+			return (NULL);
+		}
+		if (access(full_path, X_OK) == 0)
+		{
+			free_array(directories);
+			return (full_path);
+		}
+		free(full_path);
+		i++;
+	}
+	free_array(directories);
+	return (NULL);
+}
